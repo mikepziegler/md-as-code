7f4ce2574addd082a3f548bddc6c16d4c622c198
MikeZyeman <mi.zyeman@gmail.com>
2021-10-04 20:39:40 +0200

    Added 2 test files

---
+++ test/syntax.test.js
@@ -0,0 +1,21 @@
+var assert = require('assert');
+
+var MD = require('md-as-code');
+
+
+describe('Syntax Test', function() {
+
+    describe('start MD()', function() {
+        it('should return -1 when the value is not present', function() {
+
+            try {
+
+                const md = new MD("test");
+            } catch (e) {
+
+                assert.ok(false);
+
+            }
+        });
+    });
+});
